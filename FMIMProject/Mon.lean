87ceed5176c1a79271e2bb1bb0b784712c889220
Max <max.vangent@gmail.com>
2026-04-30 11:48:37 +0200

    Add statement of monoid object => commutative monoid

--- FMIMProject/Mon.lean
+++ FMIMProject/Mon.lean
@@ -1,1 +1,12 @@
 module
+import Mathlib
+
+instance : CategoryTheory.CartesianMonoidalCategory MonCat := by
+  apply CategoryTheory.CartesianMonoidalCategory.ofChosenFiniteProducts
+  sorry
+  sorry
+
+instance : CategoryTheory.BraidedCategory MonCat := sorry
+
+
+theorem commutative_monoid_of_monoid_object (M : CategoryTheory.Mon MonCat) : CategoryTheory.IsCommMonObj M.X := sorry
