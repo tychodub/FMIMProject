9c3d5e783f73182acefb8e0d133f7af7fb418b4b
Max <max.vangent@gmail.com>
2026-05-07 11:34:11 +0200

    MonCat CartesianMonoidalCategory instance

--- FMIMProject/Mon.lean
+++ FMIMProject/Mon.lean
@@ -1,10 +1,33 @@
 module
 import Mathlib
+open CategoryTheory
+open Limits
+
+def binaryProductCone (M N : MonCat) : BinaryFan M N := by
+  apply BinaryFan.mk
+  · exact (MonCat.ofHom (MonoidHom.fst M N))
+  · exact (MonCat.ofHom (MonoidHom.snd M N))
+
+
+def terminalLimitCone : IsTerminal (MonCat.of PUnit) := by
+  fapply IsTerminal.ofUniqueHom
+  · intro M
+    apply MonCat.ofHom
+    exact default
+  · intros M f
+    ext
 
 instance : CategoryTheory.CartesianMonoidalCategory MonCat := by
   apply CategoryTheory.CartesianMonoidalCategory.ofChosenFiniteProducts
-  sorry
-  sorry
+  · exact ⟨_, terminalLimitCone⟩
+  intros M N
+  fapply LimitCone.mk
+  · apply binaryProductCone
+  apply BinaryFan.IsLimit.mk _ (fun l r ↦ MonCat.ofHom (MonoidHom.prod l.hom r.hom))
+  · intros; rfl
+  · intros; rfl
+  cat_disch
+
 
 instance : CategoryTheory.BraidedCategory MonCat := sorry
 
