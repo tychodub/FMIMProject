b1377a547c244e4d861c34b7995b7decae9f5e1f
tychodub <t.b.dubbeling@student.utwente.nl>
2026-05-12 13:49:31 +0200

    Added LaxMonoidal instance for the forgetfull functor from GrpCat to MonCat, proved that said forgetfull functor reflects commutativity of monoid objects

--- FMIMProject/Grp.lean
+++ FMIMProject/Grp.lean
@@ -6,20 +6,37 @@ import Mathlib.Algebra.Category.Grp.Basic
 import Mathlib.CategoryTheory.Monoidal.ofHasFiniteProducts
 import Mathlib.Algebra.Category.Grp.CartesianMonoidal
 import Mathlib.Algebra.Category.Grp.Adjunctions
+import Mathlib.Algebra.Category.Grp.Limits
+import FMIMProject.Mon
 
 open CategoryTheory
 
--- I could not find the instance of CartesianMonoidalCategory for MonCat, if it exists
-noncomputable instance MonCatCart : CartesianMonoidalCategory MonCat := CartesianMonoidalCategory.ofHasFiniteProducts
-noncomputable instance : BraidedCategory MonCat := BraidedCategory.ofCartesianMonoidalCategory
-
 instance : Functor.LaxMonoidal (forget₂ GrpCat MonCat) where
-    ε := sorry
-    μ := sorry
+    ε := by
+        exact 𝟙 (MonoidalCategoryStruct.tensorUnit MonCat)
+    μ := by
+        exact fun X Y ↦
+          𝟙
+            (MonoidalCategoryStruct.tensorObj ((forget₂ GrpCat MonCat).obj X)
+              ((forget₂ GrpCat MonCat).obj Y))
+
 
 lemma forget_reflects_commutativity
       (M : GrpCat)
       [MonObj M] :
       (@IsCommMonObj _ _ _ _ ((forget₂ GrpCat MonCat).obj M)
        (Functor.monObjObj M))
-      → IsCommMonObj M := sorry
+      → IsCommMonObj M := by
+        intro forgetComm
+        refine { mul_comm := ?_ }
+        have isComm2 : (forget₂ GrpCat MonCat).map (β_ M M).hom ≫
+               (forget₂ GrpCat MonCat).map MonObj.mul =
+               (forget₂ GrpCat MonCat).map MonObj.mul := by
+               apply forgetComm.mul_comm
+        have : (forget₂ GrpCat MonCat).map (β_ M M).hom ≫
+               (forget₂ GrpCat MonCat).map MonObj.mul =
+               (forget₂ GrpCat MonCat).map ((β_ M M).hom ≫ MonObj.mul) := by
+                   simp
+        rw [this] at isComm2
+        apply forget₂_faithful.map_injective at isComm2
+        assumption
