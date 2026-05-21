f410f3288b3ec329d4e8a047ba4439674ff4569c
Max <max.vangent@gmail.com>
2026-05-21 12:39:42 +0200

    progress on monoid part

--- FMIMProject/Mon.lean
+++ FMIMProject/Mon.lean
@@ -5,8 +5,11 @@ public import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
 public import Mathlib.Algebra.Group.Prod
 public import Mathlib.CategoryTheory.Monoidal.Cartesian.Basic
 public import Mathlib.CategoryTheory.Monoidal.Mon_
+public import Mathlib.CategoryTheory.Monoidal.Cartesian.Mon_
+import Mathlib.GroupTheory.EckmannHilton
 open CategoryTheory
 open Limits
+open MonObj
 
 @[expose] public section
 
@@ -39,8 +42,38 @@ instance : CartesianMonoidalCategory MonCat := by
 instance : BraidedCategory MonCat := BraidedCategory.ofCartesianMonoidalCategory
 instance : SymmetricCategory MonCat := SymmetricCategory.mk
 
+-- instance (M : Mon MonCat) : MulOneClass M.X where
+--   one := M.mon.one 1
+--   mul X Y := M.mon.mul ⟨ X , Y ⟩
+--   one_mul a := sorry
+--   mul_one := sorry
+
+-- lemma swap_muls (M : Mon MonCat) (a b c d : M.X) : M.mon.mul (a * b , c * d) = (M.mon.mul (a,c)) * (M.mon.mul (b,d)) := by
+--   apply M.mon.one.hom'.map_mul
+
+-- instance (M : Mon MonCat) : MulOneClass M.X := b
+
+-- lemma ones_coincide (M : Mon MonCat) : M.mon.one 1 = 1 := by
+--   apply M.mon.one.hom'.map_one
+
+-- instance monobjinstance (M : Mon MonCat) (Y : MonCat) : Mul (Y ⟶ M.X) where
+--   mul := by
+--     intros f g
+--     apply MonCat.instCategory.comp
+--     · exact CartesianMonoidalCategory.lift f g
+--     · exact M.mon.mul
+
+-- instance (M : Mon MonCat) (Y : MonCat) : MulOneClass (Y ⟶ M.X) where
+--   one := _
+--   mul := _
+--   one_mul := _
+--   mul_one := _
 
 theorem commutative_monoid_of_monoid_object (M : Mon MonCat) : IsCommMonObj M.X := by
-  apply IsCommMonObj.mk
-  ext XY
-  sorry
+  rw [isCommMonObj_iff_isMulCommutative]
+  intro Y
+  let : CommMonoid (Y ⟶ M.X) := by
+    apply EckmannHilton.commMonoid
+
+    sorry
+  exact this.to_isCommutative
