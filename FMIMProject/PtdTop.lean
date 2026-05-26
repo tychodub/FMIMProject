5cd1c8fe410f36c8686e060f44d889c62c121974
Thomas van Maaren <thomas@Thomass-MacBook-Air.local>
2026-05-26 11:18:15 +0200

    Progress request from Max

--- FMIMProject/PtdTop.lean
+++ FMIMProject/PtdTop.lean
@@ -4,24 +4,29 @@ public import Mathlib.Topology.Defs.Basic
 public import Mathlib.CategoryTheory.Category.Basic
 public import Mathlib.Topology.ContinuousMap.Basic
 public import Mathlib.Topology.Category.TopCat.Basic
+public import Mathlib.CategoryTheory.Limits.Shapes.Products
+public import Mathlib.Topology.Category.TopCat.Limits.Products
 
 @[expose] public section
 
 open CategoryTheory Topology
 
+universe v u w
+
 class PointedTopologicalSpace (X : Type u) (x : X) where
   protected isTop : TopologicalSpace X
 
 public structure PtdTopCat where
-  off ::
+  of ::
   top : TopCat.{u}
   point : top
 
 public instance : CoeSort (PtdTopCat) (Type u) :=
   ⟨fun x ↦ PtdTopCat.top x⟩
 
-
+@[ext]
 public structure Hom (X Y : PtdTopCat) where
+  of ::
   hom' : X.top⟶ Y.top
   presPoint : hom' (X.point) = Y.point
 
@@ -33,3 +38,43 @@ public instance : Category PtdTopCat where
     refine ⟨f.hom' ≫ g.hom', ?_⟩
     simp
     rw [f.presPoint,g.presPoint]
+
+abbrev piπ {ι : Type v} (α : ι → PtdTopCat.{max v u}) (i : ι) : PtdTopCat.of (TopCat.of (∀ i, α i))
+(fun i:ι ↦ (α i).point  ) ⟶ α i where
+  hom' := by
+    apply TopCat.piπ
+  presPoint := by
+    simp
+
+def piFan {ι : Type v} (α : ι → PtdTopCat.{max v u} ) : CategoryTheory.Limits.Fan α :=
+  CategoryTheory.Limits.Fan.mk (PtdTopCat.of (TopCat.of (∀ i, α i)) (fun i:ι ↦ (α i).point  )) (piπ.{v, u} α)
+def piFanIsLimit {ι : Type v} (α : ι → PtdTopCat.{max v u}) : CategoryTheory.Limits.IsLimit (piFan α) where
+  lift S := Hom.of (TopCat.ofHom
+    {toFun:= fun s i => (S.π.app ⟨i⟩).hom' s
+     continuous_toFun := continuous_pi (fun i => (S.π.app ⟨i⟩).hom'.hom.2)}) (by simp; change ((fun s i ↦ (ConcreteCategory.hom (S.π.app { as := i }).hom') s) S.pt.point =(piFan α).pt.point );simp; ext i; have := (S.π.app { as := i }).presPoint; simp at this; exact this)
+  uniq := by
+    intro S m h
+    cases m with
+    | of mh mp =>
+    have : mh = TopCat.ofHom
+      { toFun := fun s i ↦ (ConcreteCategory.hom (S.π.app { as := i }).hom') s,
+        continuous_toFun := continuous_pi fun i ↦
+          (TopCat.Hom.hom (S.π.app { as := i }).hom').continuous_toFun } := by
+      ext x
+      funext i
+      have h' : ∀ (j : Discrete ι), mh ≫ ((piFan α).π.app j).hom' = (S.π.app j).hom' := by
+        intro j
+        calc
+          mh ≫ ((piFan α).π.app j).hom' =
+            ({ hom' := mh, presPoint := mp }: S.pt ⟶ (piFan α).pt).hom' ≫ ((piFan α).π.app j).hom'
+            := by rfl
+          _ = (({ hom' := mh, presPoint := mp }: S.pt ⟶ (piFan α).pt) ≫ ((piFan α).π.app j)).hom'
+            := by rfl
+          _ = (S.π.app j).hom' := by rw [h j]
+      simp only [Discrete.functor_obj_eq_as, Functor.const_obj_obj, ConcreteCategory.hom_ofHom]
+      change (((ConcreteCategory.hom mh) x i) =
+        (fun s i ↦ (ConcreteCategory.hom (S.π.app { as := i }).hom') s) x i)
+      simp [← h' ⟨i⟩]
+      rfl
+    subst this
+    simp
--- FMIMProject/TopGrp.lean
+++ FMIMProject/TopGrp.lean
@@ -6,10 +6,16 @@ public import Mathlib.Algebra.Category.Grp.Basic
 public import Mathlib.Topology.Homotopy.Path
 public import Mathlib.AlgebraicTopology.FundamentalGroupoid.Basic
 public import Mathlib.AlgebraicTopology.FundamentalGroupoid.FundamentalGroup
+public import Mathlib.CategoryTheory.Limits.IsLimit
+public import Mathlib.CategoryTheory.Limits.Shapes.Products
 
 public import FMIMProject.PtdTop
 
 open CategoryTheory
+
+universe u v
+
+variable {I : Type u} (X : I → PtdTopCat.{u})
 
 @[simp]
 lemma FundamentalGroup.map_id (X: Type*) (x : X) [TopologicalSpace X] :
@@ -67,5 +73,37 @@ noncomputable def fundamentalGroupFunctor : PtdTopCat ⥤ GrpCat where
     rw[←FundamentalGroup.mapOfEq_comp]
     rfl
 
-lemma preservesProduct (X : I → PtdTopCat ): Limits.PreservesLimit (Discrete.functor X) fundamentalGroupFunctor := by
+noncomputable def proj (i : I) : fundamentalGroupFunctor.obj (PtdTopCat.of (TopCat.of (∀ i, X i)) (fun i:I ↦ (X i).point  )) ⟶ fundamentalGroupFunctor.obj (X i) :=
+  fundamentalGroupFunctor.map (Hom.of ⟨_, continuous_apply i⟩ rfl)
+
+noncomputable def coneDiscreteComp :
+    Limits.Cone (Discrete.functor X ⋙ fundamentalGroupFunctor) ≌ Limits.Cone (Discrete.functor fun i => fundamentalGroupFunctor.obj (X i)) :=
+  Limits.Cone.postcomposeEquivalence (Discrete.compNatIsoDiscrete X fundamentalGroupFunctor)
+
+theorem coneDiscreteComp_obj_mapCone :
+    (coneDiscreteComp X).functor.obj (Functor.mapCone fundamentalGroupFunctor (piFan X)) =
+      Limits.Fan.mk (fundamentalGroupFunctor.obj (PtdTopCat.of (TopCat.of (∀ i, X i)) (fun i:I ↦ (X i).point  ))) (proj X) :=
+  rfl
+
+def piProj
+    {I : Type}
+    (X : I → GrpCat)
+    (b : I) :
+    GrpCat.of ((i : I) → X i) →* X b :=⟨
+    { toFun := fun f => f b,
+      map_one' := rfl},(by intro x y; rfl)
+  ⟩
+-- (piProj (fun (b:I) => fundamentalGroupFunctor.obj (X b)))
+
+-- def piTopToPiCone :
+--     Limits.Fan.mk (fundamentalGroupFunctor.obj (PtdTopCat.of (TopCat.of (∀ i, X i)) (fun i:I ↦ (X i).point  ))) (proj X) ⟶
+--       CategoryTheory.Limits.Fan.mk (GrpCat.of (∀i, fundamentalGroupFunctor.obj (X i))) (by ) where
+--   hom := CategoryTheory.Functor.pi' (proj X)
+
+lemma preservesProduct : Limits.PreservesLimit (Discrete.functor X) fundamentalGroupFunctor := by
+  apply Limits.preservesLimit_of_preserves_limit_cone (piFanIsLimit X)
+  apply (Limits.IsLimit.ofConeEquiv (coneDiscreteComp X)).toFun
+  simp only [coneDiscreteComp_obj_mapCone]
   sorry
+
+  --apply Limits.IsLimit.ofIsoLimit _ (asIso (piTopToPiCone X)).symm
